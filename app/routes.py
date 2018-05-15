b425bdc0ace3b528bf4b5f2f44c868e23e7c2f22
Jaideep Singh <jaideep2@illinois.edu>
2018-05-15 01:48:38 -0400

    v0.2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "HELLO WORD"
+    user = {'username':'JD'}
+    posts = [
+        {
+            'author' : { 'username':'AB' },
+            'body' : 'What is life but a reason to live fully?'
+        },
+        {
+            'author' : { 'username':'VK' },
+            'body' : 'History has shown that huslers will hustle'
+        }
+    ]
+    return render_template('index.html',title='home',user=user, posts=posts)
