af8c3c1914e94cdcdc383d9d8ff76cd71398a2e6
Jaideep Singh <jaideep2@illinois.edu>
2018-06-26 09:32:16 -0400

    followers and moar tests

--- app/models.py
+++ app/models.py
@@ -8,6 +8,12 @@ from app import login
 def load_user(id):
     return User.query.get(int(id))
 
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,6 +22,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,8 +37,39 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def avatar(self):
-        pass
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        '''
+        The is_following() method issues a query on the followed relationship to check if a link between two users
+        already exists. You have seen me use the filter_by() method of the SQLAlchemy query object before, for example
+        to find a user given its username. The filter() method that I'm using here is similar, but lower level, as it
+        can include arbitrary filtering conditions, unlike filter_by() which can only check for equality to a constant
+        value. The condition that I'm using in is_following() looks for items in the association table that have the
+        left side foreign key set to the self user, and the right side set to the user argument. The query is terminated
+         with a count() method, which returns the number of results. The result of this query is going to be 0 or 1, so
+         checking for the count being 1 or greater than 0 is actually equivalent. Other query terminators you have seen
+         me use in the past are all() and first().
+        '''
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        '''
+        Followed format: Post.query.join(...).filter(...).order_by(...)
+        https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-viii-followers
+        '''
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -1,4 +1,4 @@
-flask db init
+#flask db init
 #flask db migrate -m "Users table"
 #flask db migrate -m "Posts table"
-flask db upgrade
+#flask db upgrade
